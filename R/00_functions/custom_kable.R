5f88e1d7cce6f3731796ed736dc6e6cce3c78b06
Glenn Patrick Williams <33417082+gpwilliams@users.noreply.github.com>
2020-06-23 22:48:12 +0100

    updated results with minor change to table function
    custom_kable() now allows inclusion of an optional caption. Results now fully uptated with figure and table captions.

--- R/00_functions/custom_kable.R
+++ R/00_functions/custom_kable.R
@@ -1,6 +1,9 @@
-custom_kable <- function(data, add_footer = FALSE, ...) {
+custom_kable <- function(data, caption = NULL, add_footer = FALSE, ...) {
   #' Custom styling and presentation applied to kable tables.
   #' @param data A data.frame to be displayed in kable.
+  #' @param caption optional string to include as a caption.
+  #' @param add_footer Logical for whether or not to add a footer.
+  #' @param ... String for the footer to be added.
   #' @return A kable formatted data.frame for display in html documents 
   #'     with custom styling.
   #' @examples
@@ -9,14 +12,14 @@ custom_kable <- function(data, add_footer = FALSE, ...) {
   
   if(add_footer == FALSE) {
     data %>% 
-      kableExtra::kable(digits = 3, escape = FALSE) %>%
+      kableExtra::kable(digits = 3, escape = FALSE, caption = caption) %>%
       kableExtra::kable_styling(
         bootstrap_options = c("striped", "hover"), 
         full_width = TRUE
       )
   } else {
     data %>% 
-      kableExtra::kable(digits = 3, escape = FALSE) %>%
+      kableExtra::kable(digits = 3, escape = FALSE, caption = caption) %>%
       kableExtra::kable_styling(
         bootstrap_options = c("striped", "hover"), 
         full_width = TRUE
